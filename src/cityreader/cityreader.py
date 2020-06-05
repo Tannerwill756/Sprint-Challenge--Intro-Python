75fda919c18eb8543e079d661faec24286e51599
Tanner Williams <tannerwill756@gmail.com>
2020-06-05 13:14:33 -0400

    mvp

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -29,8 +29,7 @@ cities = []
 
 
 def cityreader(cities=[]):
-    # TODO
-    # Implement the functionality to read from the 'cities.csv' file
+    # TODO Implement the functionality to read from the 'cities.csv' file
     # For each city record, create a new City instance and add it to the
     # `cities` list
 
